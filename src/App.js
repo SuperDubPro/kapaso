b4f421e44ba1fc5fa9ad38a4719783c94805bac5
Nickolay Dubinin <SuperDubPro@gmail.com>
2022-08-21 03:24:17 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react'
-import { ColorModeSwitcher } from './ColorModeSwitcher'
 import { Logo } from './Logo'
+import { Navbar } from './components'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
+        <Navbar />
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
---
+++ src/components/Navbar/MobileNav/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Stack, useColorModeValue } from '@chakra-ui/react'
+import NAV_ITEMS from '../data'
+import MobileNavItem from '../MobileNavItem'
+
+export default function MobileNav() {
+  return (
+    <Stack
+      bg={useColorModeValue('white', 'gray.800')}
+      p={4}
+      display={{ md: 'none' }}
+    >
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  )
+}
