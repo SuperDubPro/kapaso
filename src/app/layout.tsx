c7f5030cce983f0962f3686bb82efc20cb62e62f
Nickolay Dubinin <superdubpro@gmail.com>
2025-03-20 00:14:57 +0300

    next theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Inter } from "next/font/google"
+import Provider from "./provider"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,9 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html className={inter.className} suppressHydrationWarning>
+      <head />
+      <body>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
