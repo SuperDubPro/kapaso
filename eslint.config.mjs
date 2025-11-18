7583bcf833dfce10b358dce0a5e27b7796386d17
Nickolay Dubinin <superdubpro@gmail.com>
2025-11-19 00:53:48 +0300

    migration to next 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import { dirname } from "path"
 import { fileURLToPath } from "url"
 
 import { FlatCompat } from "@eslint/eslintrc"
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -10,7 +13,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
   ...compat.config({
     extends: [
       "next/core-web-vitals",
@@ -63,6 +68,14 @@ const eslintConfig = [
       }],
     },
   }),
-]
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
 
-export default eslintConfig
+export default eslintConfig;
